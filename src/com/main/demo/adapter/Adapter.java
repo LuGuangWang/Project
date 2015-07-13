c05789996ab8c99a7a494a89fc8667a467d67255
wangluguang <wangluguang@jt-xxgl-wangluguang.staff.neworiental.org>
2015-07-13 11:28:39 +0800

    wlg 适配器模式

---
+++ src/com/main/demo/adapter/Adapter.java
@@ -0,0 +1,28 @@
+package com.main.demo.adapter;
+
+/*
+ * 适配器类
+ * 以下情况比较适合使用 Adapter 模式：
+ * 1.当你想使用一个已经存在的类，而它的接口不符合你的需求；
+ * 2.你想创建一个可以复用的类，该类可以与其他不相关的类或不可预见的类协同工作；
+ * 3.你想使用一些已经存在的子类，但是不可能对每一个都进行子类化以匹配它们的接口，对象适配器可以适配它的父亲接口。
+ */
+public class Adapter implements Target{
+	 /*
+	 * 持有需要被适配的接口对象
+	 */
+	private Adaptee adaptee;
+	/*
+	 * 构造方法，传入需要被适配的对象
+	 * @param adaptee 需要被适配的对象
+	 */
+	public Adapter(Adaptee adaptee){
+	this.adaptee = adaptee;
+	}
+	@Override
+	public void request() {
+		// TODO Auto-generated method stub
+		adaptee.specificRequest();
+	}
+ 
+}
---
+++ src/com/main/demo/adapter/AdapterUtil.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.main.demo.adapter;
+
+/**
+ * 适配器模式
+ * 使用适配器的客户端
+ * @author wangluguang
+ *
+ */
+public class AdapterUtil {
+
+	public static void main(String[] args){
+		 //创建需要被适配的对象
+		 Adaptee adaptee = new Adaptee();
+		 //创建客户端需要调用的接口对象
+		 Target target = new Adapter(adaptee);
+		 //请求处理
+		 target.request();
+	 }
+}
