32eb7161ace3b89e597c5f6e9a9274ef5dcd8436
wangluguang <wangluguang@jt-xxgl-wangluguang.staff.neworiental.org>
2015-07-15 20:44:26 +0800

    wlg 重写equal

---
+++ src/com/main/util/set/DataBean.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.main.util.set;
+
+/**
+ * @author wangluguang
+ *
+ */
+public class DataBean {
+	private String name;
+	private String value;
+	
+	DataBean(String name,String value){
+		this.name = name;
+		this.value = value;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public String toString(){
+		return name+":"+value;
+	}
+	/**
+	 * 	重写equals方法的要求：
+	 *	1.自反性：对于任何非空引用x，x.equals(x)应该返回true。
+	 *	2.对称性：对于任何引用x和y，如果x.equals(y)返回true，那么y.equals(x)也应该返回true。
+	 *	3.传递性：对于任何引用x、y和z，如果x.equals(y)返回true，y.equals(z)返回true，那么x.equals(z)也应该返回true。
+	 *	4.一致性：如果x和y引用的对象没有发生变化，那么反复调用x.equals(y)应该返回同样的结果。
+	 *	5.非空性：对于任意非空引用x，x.equals(null)应该返回false。
+	 */
+    public boolean equals(Object obj) {  
+        if(this == obj)  
+            return false;  
+        if(obj == null)  
+            return false;  
+        if(getClass() != obj.getClass() )  
+            return false;  
+        DataBean other = (DataBean)obj;  
+        if(name == null) {  
+             if(other.name != null) {  
+                  return false;  
+             }  
+        }else if(!name.equals(other.name))  
+             return false;
+        return true;  
+    }
+    
+    public int hashCode(){
+    	return this.name.hashCode();
+    }
+}
--- src/com/main/util/test/Test.java
+++ src/com/main/util/test/Test.java
@@ -1,32 +1,55 @@
 package com.main.util.test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Test{
   Test(){}
   
   
   
-  public static void main(String[] args){
-    System.out.println("this is my fisrt github source code.");
-    MyInterface in = new Interface2();
-    in.test();
-    
-    String a = "A_B_C_D";
-    String[] strs = a.split("_");
-    for(String str :strs)
-    	System.out.println(str);
-    
-    String time = "2015-01-01";
-    int length = time.length();
-    System.out.println(time + " " + length + " "+ (time.length()==10));
-    
-    if(time.length()>10){
-    	time = time.substring(0,10)+" 23:59:59";
-    }
-    else{
-    	time += " 23:59:59";
-    }
-    length = time.length();
-    System.out.println(time + " " + length);
-    
+  public static void main(String[] args) throws Exception{
+//    System.out.println("this is my fisrt github source code.");
+//    MyInterface in = new Interface2();
+//    in.test();
+//    
+//    String a = "A_B_C_D";
+//    String[] strs = a.split("_");
+//    for(String str :strs)
+//    	System.out.println(str);
+//    
+//    String time = "2015-01-01";
+//    int length = time.length();
+//    System.out.println(time + " " + length + " "+ (time.length()==10));
+//    
+//    if(time.length()>10){
+//    	time = time.substring(0,10)+" 23:59:59";
+//    }
+//    else{
+//    	time += " 23:59:59";
+//    }
+//    length = time.length();
+//    System.out.println(time + " " + length);
+	  
+	  
+//	  SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//	  Calendar cal = Calendar.getInstance();
+//	  cal.setTime(df.parse(df.format(new Date())));
+//	  cal.add(Calendar.DATE, -60);
+//	  System.out.println(df.format(cal.getTime()));
+//	  
+//	  int year = Calendar.getInstance().get(Calendar.YEAR);
+//	  System.out.println(year);
+	  
+	  
+	  String str = "尖1子1尖1子2目标";
+	  System.out.println(str.contains("尖子"));
+	  System.out.println(str.contains("目标"));
+	  
+	  
+	  
+	  
   }
 }
