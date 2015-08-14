b3ea1ca61aa88c8bf37c2b632832cb633ece74ab
wangluguang <wangluguang@jt-xxgl-wangluguang.staff.neworiental.org>
2015-08-14 18:26:09 +0800

    wlg 使用新日期

---
+++ src/com/main/demo/date/NewDateClassUtil.java
@@ -0,0 +1,27 @@
+package com.main.demo.date;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+
+public class NewDateClassUtil {
+
+  public static void testNewDate(){
+    LocalDate localDate = LocalDate.now();
+    System.out.println(localDate);
+  }
+  /**
+   * 获取日期
+   */
+  public static void testFormatDate() throws ParseException{
+    String datetime = "2015-08-11";
+    LocalDate localDate = LocalDate.parse(datetime);
+    localDate = localDate.minusDays(1L);
+    System.out.println(localDate.getDayOfWeek().getValue());
+  }
+  
+  public static void main(String[] args) throws ParseException {
+    testNewDate();
+    testFormatDate();
+  }
+
+}
