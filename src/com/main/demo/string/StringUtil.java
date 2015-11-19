b8c8cff0ce550c2f60c43d1c3967a4d767e8748e
wangluguang <wangluguang@jt-xxgl-wangluguang.staff.neworiental.org>
2015-11-19 16:48:45 +0800

    wlg

--- src/com/main/demo/string/StringUtil.java
+++ src/com/main/demo/string/StringUtil.java
@@ -6,11 +6,28 @@ public class StringUtil {
   static void replaceChinese(){
     String str = "*%hell^你好12）（";
     str = str.replaceAll("[^\u4E00-\u9FA5]", "");
-    System.out.print(str);
+    System.out.println(str);
+  }
+  //是否是中文
+  static boolean isChinese(String str){
+    return str.matches("[\u4E00-\u9FA5]*");
+  }
+  
+  //split
+  static void splitStr(){
+    String str = "TECH00112/李小双";
+    String[] strs = str.split("/");
+    for(String s:strs){
+      if(isChinese(s))
+        System.out.println("name:"+s);
+      else
+        System.out.println("code:"+s);
+    }
   }
   
   public static void main(String[] args) {
     replaceChinese();
+    splitStr();
   }
 
 }
