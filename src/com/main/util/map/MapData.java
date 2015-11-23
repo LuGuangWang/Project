9e7490115c72002fb2e563f80548b29a560de9e2
wangluguang <wangluguang@jt-xxgl-wangluguang.staff.neworiental.org>
2015-11-23 17:26:39 +0800

    wlg

---
+++ src/com/main/util/map/MapData.java
@@ -0,0 +1,21 @@
+package com.main.util.map;
+
+public class MapData {
+  private String name;
+  private String sex;
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getSex() {
+    return sex;
+  }
+  public void setSex(String sex) {
+    this.sex = sex;
+  }
+  public String toString(){
+    return name;
+  }
+}
--- src/com/main/util/map/MapUtil.java
+++ src/com/main/util/map/MapUtil.java
@@ -5,7 +5,30 @@ import java.util.Map;
 
 public class MapUtil {
 
+  public static void testMapObject(){
+    Map<String,MapData> map = new HashMap<String, MapData>();
+    MapData data = new MapData();
+    data.setName("LG");
+    map.put("key", data);
+    System.out.println(map.get("key"));
+    data.setName("LG1");
+    System.out.println(map.get("key"));
+  }
+  
+  public static void testMapString(){
+    Map<String,String> map = new HashMap<String, String>();
+    MapData data = new MapData();
+    data.setName("LG");
+    map.put("key", data.toString());
+    System.out.println("String key: "+map.get("key"));
+    data.setName("LG1");
+    map.put("key", data.toString());
+    System.out.println("String key: "+map.get("key"));
+  }
+  
 	public static void main(String[] args) {
+	  testMapObject();
+	  testMapString();
 		// TODO Auto-generated method stub
 		Map<String,String> map1 = new HashMap<String, String>();
 		Map<String,String> map2 = new HashMap<String, String>();
