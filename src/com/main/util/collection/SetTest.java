8415f574b30d18904624eed23dca19756214b04f
wangluguang <wangluguang@jt-xxgl-wangluguang.staff.neworiental.org>
2015-12-03 15:24:58 +0800

    wlg set collection test

---
+++ src/com/main/util/collection/SetTest.java
@@ -0,0 +1,34 @@
+package com.main.util.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SetTest {
+  /**
+   * 
+   */
+  static void testMultipleElement(){
+    List<String> arr = Arrays.asList("1","1","2");
+    Set<String> set = new HashSet<>(arr);
+//    set.add("1");
+//    set.add("1");
+//    set.addAll(arr);
+    List<String> arrnew = new ArrayList<String>(set);
+    
+    for(String s:set){
+      System.out.println("set element: "+s);
+    }
+    for(String e:arrnew){
+      System.out.println("list element: "+e);
+    }
+  }
+  
+  
+  public static void main(String[] args) {
+    testMultipleElement();
+  }
+
+}
