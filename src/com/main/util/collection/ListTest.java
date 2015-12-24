03e90ec5f0fbf99baa9008a83f9610e141013d6d
wangluguang <wangluguang@jt-xxgl-wangluguang.staff.neworiental.org>
2015-12-24 17:57:26 +0800

    wlg

--- src/com/main/util/collection/ListTest.java
+++ src/com/main/util/collection/ListTest.java
@@ -4,7 +4,20 @@ import java.util.*;
 
 public class ListTest {
 
+  static void clear(){
+    Bean b = new Bean();
+    b.setTest("tst1");
+    List<Bean> arr = new ArrayList<Bean>();
+    arr.add(b);
+    List<Bean> arr2 = new ArrayList<Bean>();
+    arr2.addAll(arr);
+    arr.clear();
+    System.out.println("result:" + arr2);
+  }
+  
 	public static void main(String[] args) {
+	  clear();
+	  
 		Bean b = new Bean();
 		b.setTest("test");
 		List<Bean> beans = new ArrayList<Bean>();
