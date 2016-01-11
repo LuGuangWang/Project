c02c3449d9d03f3487910347ba5f4bed64a039fb
wangluguang <wangluguang@jt-xxgl-wangluguang.staff.neworiental.org>
2016-01-11 13:29:33 +0800

    wlg

--- src/com/main/demo/string/StringUtil.java
+++ src/com/main/demo/string/StringUtil.java
@@ -10,7 +10,7 @@ public class StringUtil {
   }
   //是否是中文
   static boolean isChinese(String str){
-    return str.matches("[\u4E00-\u9FA5]*");
+    return str.replaceAll("[\u4E00-\u9FA5]*", "").length()==0;
   }
   
   //split
@@ -18,9 +18,9 @@ public class StringUtil {
     String str = "TECH00112/李小双";
     String[] strs = str.split("/");
     for(String s:strs){
-      if(isChinese(s))
-        System.out.println("name:"+s);
-      else
+      if(isChinese(s)){
+        System.out.println("chinese name:"+s);
+      }else
         System.out.println("code:"+s);
     }
   }
@@ -52,13 +52,20 @@ public class StringUtil {
     System.out.println("date:"+date);
   }
   
+  static void matchTO(){
+    String str = "2016.6.6-2016.7.1周一至周五9:00-12:00,13:30-16:30,17:30-20:30";
+    String result = str.substring(0, str.length()-str.replaceFirst("^\\d+\\.\\d+\\.\\d+-\\d+\\.\\d+\\.\\d+", "").length());
+    System.out.println("this result is matched:"+result);
+  }
+  
   public static void main(String[] args) {
 //    replaceKuohao();
 //    compareValue();
 //    replaceChinese();
 //    splitStr();
+    matchTO();
 //    replaceSpot();
-    matchDate();
+//    matchDate();
   }
 
 }
