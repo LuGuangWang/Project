48d0e6709475ba5878b3ccab39c9f2912f6adba0
wangluguang <wangluguang@jt-xxgl-wangluguang.staff.neworiental.org>
2016-02-25 14:30:15 +0800

    wlg abstract class

---
+++ src/com/main/util/test/AbstractTest.java
@@ -0,0 +1,11 @@
+package com.main.util.test;
+
+abstract public class AbstractTest {
+  AbstractTest(){
+    System.out.println("parent class constructor");
+  }
+  
+  public void test(){
+    System.out.println("parent class test function.");
+  }
+}
---
+++ src/com/main/util/test/AbstractTest2.java
@@ -0,0 +1,21 @@
+package com.main.util.test;
+
+
+public class AbstractTest2 extends AbstractTest {
+
+  public AbstractTest2() {
+    super();
+    System.out.println("sub class constructor");
+  }
+  
+  public void test(){
+    super.test();
+    System.out.println("sub class test function.");
+  }
+
+  public static void main(String[] args) {
+    AbstractTest2 t = new AbstractTest2();
+    t.test();
+  }
+
+}
