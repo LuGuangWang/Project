2a57e63bd628d3b35c760d6e86c1b000fc553a8c
wangluguang <wangluguang@jt-xxgl-wangluguang.staff.neworiental.org>
2016-03-01 16:44:43 +0800

    wlg http post

--- src/com/main/solr/SolrSearch.java
+++ src/com/main/solr/SolrSearch.java
@@ -1,6 +1,8 @@
 package com.main.solr;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
@@ -9,6 +11,7 @@ import org.apache.solr.common.SolrInputDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.main.http.HttpUtil;
 import com.main.solr.bean.ResultBean;
 
 public class SolrSearch {
@@ -24,7 +27,7 @@ public class SolrSearch {
 //    String field2 = " -area_name:临时教室校区";//不包含
     
     
-    String field2 = " (area_name:临时 OR area_name:新东方总部)";//不包含
+    String field2 = " (area_name:临时 OR area_name:新东方总部)";
     String q = localParams + field1 +field2;
     query.setQuery(q);
     try {
@@ -35,25 +38,55 @@ public class SolrSearch {
       log.info("exception:",e);
     }
   }
-  
-  
+  /**
+   * 添加
+   */
   static void testMergeIndex(){
     SolrInputDocument doc = new SolrInputDocument();
-    doc.addField("name", "LGTEST");
-    doc.addField("id_no", "19891229");
-    doc.addField("id", "19891229");
+    doc.addField("name", "LGTEST2");
+    doc.addField("id", "19891230");
     try{
       server.add(doc);
-      server.optimize(true,true);
+      server.optimize(false,false);
       server.commit();
     }catch(Exception e){
       log.info("exception:",e);
     }
   }
   
+  /**
+   * 测试批量增加
+   * @throws Exception 
+   * @throws  
+   */
+  static void testDeltaImport(){
+    String url = solrServer + "dataimport";
+    Map<String,String> param = new HashMap<String, String>();
+    param.put("command", "delta-import");
+    param.put("clean", "false");
+    param.put("commit", "true");
+    String reslut = HttpUtil.doPost(url, param);
+    log.info("----"+reslut);
+  }
   
+  /**
+   * 测试全量增加
+   * @throws Exception 
+   * @throws  
+   */
+  static void testFullImport(){
+    String url = solrServer + "dataimport";
+    Map<String,String> param = new HashMap<String, String>();
+    param.put("command", "full-import");
+    param.put("clean", "true");
+    param.put("commit", "true");
+    String reslut = HttpUtil.doPost(url, param);
+    log.info("----"+reslut);
+  }
   public static void main(String[] args){
 //    testMergeIndex();
-    testSearchQ();
+//    testSearchQ();
+//    testDeltaImport();
+    testFullImport();
   }
 }
