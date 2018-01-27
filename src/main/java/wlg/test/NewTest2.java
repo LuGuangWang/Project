0ee385e56684d88f4a29f57c9140060a553d7e7c
wangluguang <wangluguang@jd.com>
2018-01-27 17:51:56 +0800

    麻将胡牌

--- src/main/java/wlg/test/NewTest2.java
+++ src/main/java/wlg/test/NewTest2.java
@@ -1,12 +1,29 @@
 package wlg.test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class NewTest2 {
   
   public static void main(String[] args) {
-    System.out.println(testReturn());
-    
+//    System.out.println(testReturn());
+    List<Integer> arr= Arrays.asList(1,2,3,4,1,6,7,8,9);
+    arr.sort((a,b)->b-a);
+    System.out.println(count(arr,1));
 //    testCycle();
   }
+  
+  private static int count(List<Integer> arr, int pai) {
+		int count = 0;
+		for(int i=arr.size()-1;i>=0;i--) {
+			if(arr.get(i)==pai) {
+				count++;
+			}else {
+				break;
+			}
+		}
+		return count;
+	}
 
   static int testReturn() {
     int s = 1;
