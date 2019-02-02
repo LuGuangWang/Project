60172eb464777c0737237af3060bb15121777561
wangluguang <wangluguang@jd.com>
2019-02-02 16:41:22 +0800

    add

--- src/main/java/wlg/arithmetic/BlanceTree.java
+++ src/main/java/wlg/arithmetic/BlanceTree.java
@@ -1,5 +1,6 @@
 package wlg.arithmetic;
 
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * [平衡二叉树] 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
@@ -13,18 +14,44 @@ public class BlanceTree {
 	public static void main(String[] args) {
 		BlanceTree b = new BlanceTree();
 		Tree root = b.buildTree();
-		int d = b.deep(root);
-		System.out.println("\ndeep:" + d );
+//		int d = b.deep(root);
+//		System.out.println("\ndeep:" + d );
+		AtomicInteger deep = new AtomicInteger(0);
+		boolean res = b.isBalance(root, deep);
+		System.out.println("is balence tree: " + res);
+	}
+	
+	boolean isBalance(Tree root,AtomicInteger deep) {
+		boolean res = true;
+		if(root != null) {
+			AtomicInteger ld = new AtomicInteger(deep.get());
+			AtomicInteger rd = new AtomicInteger(deep.get());
+			boolean lb = isBalance(root.left, ld);
+			if(lb==false) {
+				return false;
+			}
+			boolean rb = isBalance(root.righ, rd);
+			if(rb==false) {
+				return false;
+			}
+			deep.set(Math.max(ld.get(), rd.get())+1);
+			int dis = Math.abs(ld.get() - rd.get());
+			res = dis>1?false:true;
+		}else {
+			deep.set(0);
+		}
+		
+		return res;
 	}
 	
 	int deep(Tree root) {
 		int deep = 0;
 		if(root!=null) {
-			int ldeep = deep(root.left) + 1;
-			int rdeep = deep(root.righ) + 1;
+			int ldeep = deep(root.left);
+			int rdeep = deep(root.righ);
 			int dis = ldeep - rdeep;
 			System.out.println("dis:" + dis);
-			deep = Math.max(ldeep,rdeep);
+			deep = Math.max(ldeep,rdeep) + 1;
 		}else{
 			deep = 0;
 		}
