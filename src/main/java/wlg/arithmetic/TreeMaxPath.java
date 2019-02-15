d94229ca54a7679b715d4e3b6019a33db99da9d1
wangluguang <wangluguang@jd.com>
2019-02-15 11:21:02 +0800

    add

--- src/main/java/wlg/arithmetic/TreeMaxPath.java
+++ src/main/java/wlg/arithmetic/TreeMaxPath.java
@@ -3,7 +3,7 @@ package wlg.arithmetic;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * 寻找树中的最大路径和
+ * 树中的最大路径和 & 树的最大路径长度
  * @author seven
  *
  */
@@ -14,12 +14,29 @@ public class TreeMaxPath {
 		Tree root = t.buildTree();
 		AtomicInteger m = new AtomicInteger(Integer.MIN_VALUE);
 		t.maxSum(root, m);
-		System.out.println(m.get());
+		System.out.println("max: " + m.get());
+		
+		AtomicInteger mlen = new AtomicInteger(0);
+		t.maxPath(root, mlen);
+		System.out.println("max len: " + mlen.get());
 	}
-
-	int maxSum(Tree root,AtomicInteger max) {
-		if(root==null)
+	//树的最大路径长度
+	int maxPath(Tree root,AtomicInteger maxLen) {
+		if(root==null) {
 			return 0;
+		}
+		int llen = maxPath(root.left, maxLen);
+		int rlen = maxPath(root.righ, maxLen);
+		
+		int mlen = llen + rlen + 1;
+		maxLen.set(Math.max(maxLen.get(), mlen));
+		return 1 + Math.max(llen, rlen);
+	}
+	//树中的最大路径和
+	int maxSum(Tree root,AtomicInteger max) {
+		if(root==null) {
+			return 0;
+		}
 		int lsum = maxSum(root.left, max);
 		int rsum = maxSum(root.righ, max);
 		if(lsum<0) {
@@ -36,11 +53,14 @@ public class TreeMaxPath {
 	
 	Tree buildTree() {
 		Tree root = new Tree(1);
-		root.righ = new Tree(5);
+		Tree right  = new Tree(5);
 		Tree left = new Tree(2);
+		root.righ = right;
 		root.left=left;
 		left.righ = new Tree(4);
 		left.left = new Tree(3);
+		right.left = new Tree(7);
+		right.righ = new Tree(8);
 		return root;
 	}
 	
