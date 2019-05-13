301b4210418dbd6eb94787ca2b0c81225acef076
wangluguang20@sina.com <wang20081129>
2019-05-13 18:49:08 +0800

    add

--- src/main/java/wlg/webapi/WebInterceptor.java
+++ src/main/java/wlg/webapi/WebInterceptor.java
@@ -4,6 +4,8 @@ package wlg.webapi;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,9 +13,13 @@ import org.springframework.web.servlet.ModelAndView;
  * 处理跨域
  */
 public class WebInterceptor implements HandlerInterceptor {
+  private Logger log = LoggerFactory.getLogger(WebInterceptor.class);
+
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
+	log.info("===========test");  
+	  
     // 处理跨域问题 有安全隐患
     response.setHeader("X-Frame-Options", "ALLOW-FROM");
     //添加add_header时，需要指定always，否则当接口4xx和5xx时，浏览器依旧会报跨域问题
--- src/main/java/wlg/webapi/web/comfig/MvcConfig.java
+++ src/main/java/wlg/webapi/web/comfig/MvcConfig.java
@@ -1,8 +1,11 @@
 package wlg.webapi.web.comfig;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import wlg.webapi.WebInterceptor;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
@@ -13,5 +16,11 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     registry.addViewController("/").setViewName("security/home");
     registry.addViewController("/hello").setViewName("security/hello");
     registry.addViewController("/login").setViewName("security/login");
+  }
+  
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+	  registry.addInterceptor(new WebInterceptor());
+	  super.addInterceptors(registry);
   }
 }
