f35cec461527f48deac8e6c69ad3db4f551eea35
bjwangluguang <bjwangluguang@didiglobal.com>
2020-05-11 16:44:31 +0800

    add

--- src/main/java/wlg/arithmetic/LinkListSum.java
+++ src/main/java/wlg/arithmetic/LinkListSum.java
@@ -11,14 +11,100 @@ package wlg.arithmetic;
  */
 public class LinkListSum {
 
+	public static Node res = null;
+
 	public static void main(String[] args) {
 		LinkListSum t = new LinkListSum();
 
 		Node a = t.buildLinkedList(4,6);
-		Node b = t.buildLinkedList(4,5);
+		Node b = t.buildLinkedList(4,6);
 		
 		//补齐
 		t.addSum(a, b);
+
+
+		System.out.println("==============");
+		a = t.buildLinkedList(5,6);
+		b = t.buildLinkedList(5,6);
+		t.addSum1(a,b);
+		res.print();
+
+		System.out.println("==============");
+
+		a = t.buildLinkedList(7,8);
+		b = t.buildLinkedList(5,6);
+
+		a.print();
+		b.print();
+
+		Node result = t.addSum3(a,b);
+
+		result.print();
+	}
+
+	private Node addSum3(Node a,Node b){
+		Result res = new Result();
+
+		int flag = addSum2(a,b,res);
+
+		if(flag>0){
+			Node v = new Node(flag);
+			v.next = res.getHead();
+			res.setHead(v);
+		}
+		return res.getHead();
+	}
+
+	private int addSum2(Node a,Node b,Result res){
+		int flag = 0,val = 0;
+		if(a != null && b != null){
+			flag = addSum2(a.next,b.next,res);
+		}else{
+			return flag;
+		}
+
+		val = a.data + b.data + flag;
+
+		flag = val>=10?1:0;
+		val = val % 10;
+
+
+
+		Node v = new Node(val);
+		v.next = res.getHead();
+		res.setHead(v);
+
+		return flag;
+	}
+
+	class Result{
+		Node head;
+
+		public Node getHead() {
+			return head;
+		}
+
+		public void setHead(Node head) {
+			this.head = head;
+		}
+	}
+
+	private int addSum1(Node a,Node b){
+		int flag = 0,val = 0;
+
+		if(a.next!=null && b.next!=null){
+			flag = addSum1(a.next,b.next);
+		}
+
+		val = a.data + b.data + flag;
+		flag = val>=10?1:0;
+		val = val % 10;
+
+		Node v = new Node(val);
+		v.next=res;
+		res = v;
+
+		return flag;
 	}
 
 	private void addSum(Node a, Node b) {
@@ -84,9 +170,14 @@ public class LinkListSum {
 	class Node {
 		private int data;
 		private Node next;
+		private Node head;
 
 		public Node(int data) {
 			this.data = data;
+		}
+
+		public void setHead(Node head){
+			this.head = head;
 		}
 
 		public int getData() {
