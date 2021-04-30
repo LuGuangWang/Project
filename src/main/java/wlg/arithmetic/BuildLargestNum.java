0f52c2b4d61da6b6da90d223e9e5c1da02dd1b76
bjwangluguang <bjwangluguang@didiglobal.com>
2021-04-30 11:11:55 +0800

    add

--- src/main/java/wlg/arithmetic/BuildLargestNum.java
+++ src/main/java/wlg/arithmetic/BuildLargestNum.java
@@ -3,6 +3,10 @@ package wlg.arithmetic;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+ *  例如输入数组{901,9,1}，则打印出这三个数字能排成的最大数字99011
+ */
 public class BuildLargestNum {
 
     public String buildLargestNum(int[] nums){
